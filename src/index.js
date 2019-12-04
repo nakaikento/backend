c7ed542c8fd0c3ba8e8ad1094248b9b5324d56fa
nakaikento <okamoto2okamoto@gmail.com>
2019-12-04 17:16:05 +0900

    change api endpoint from "/notes" to "/api/notes"

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ let notes = [
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
-app.get('/notes', (req, res) => {
+app.get('/api/notes', (req, res) => {
   res.json(notes)
 })
 app.get('/notes/:id', (request, response) => {
