a23da849fb9210dc0e41925414fe88768ac6b6b2
nakaikento <okamoto2okamoto@gmail.com>
2019-12-04 19:43:12 +0900

    defined DB model in note.js

--- models/note.js
+++ models/note.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose')
 
 const url = process.env.MONGODB_URI
 
-console.log('connecting to', url)
-
 mongoose.connect(url, { useNewUrlParser: true })
   .then(result => {
     console.log('connected to MongoDB')
@@ -28,3 +26,24 @@ mongoose.connect(url, { useNewUrlParser: true })
   })
 
   module.exports = mongoose.model('Note', noteSchema)
+
+  // let notes = [
+  //   {
+  //     id: 1,
+  //     content: "HTML is easy",
+  //     date: "2019-05-30T17:30:31.098Z",
+  //     important: true
+  //   },
+  //   {
+  //     id: 2,
+  //     content: "Browser can execute only Javascript",
+  //     date: "2019-05-30T18:39:34.091Z",
+  //     important: false
+  //   },
+  //   {
+  //     id: 3,
+  //     content: "GET and POST are the most important methods of HTTP protocol",
+  //     date: "2019-05-30T19:20:14.298Z",
+  //     important: true
+  //   }
+  // ]
