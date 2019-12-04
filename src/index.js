bed05ab9c5c808261e2f2b152f78cf523d8bba7c
nakaikento <okamoto2okamoto@gmail.com>
2019-12-04 19:44:40 +0900

    modified. extracted Note from this file to moedls/note

--- src/index.js
+++ src/index.js
@@ -1,50 +1,28 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const Note = require('./models/note')
 
 // middleware
 app.use(bodyParser.json())
 app.use(cors())
 app.use(express.static('build'))
 
-
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true
-  }
-]
-
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
-})
 // app.get('/api/notes', (req, res) => {
-//   Note.find({}).then(notes => {
 //   res.json(notes)
-//   })
 // })
-
-
+app.get('/api/notes', (req, res) => {
+  Note.find({}).then(notes => {
+  res.json(notes)
+  })
+})
 
 app.get('/notes/:id', (request, response) => {
   const id = request.params.id
