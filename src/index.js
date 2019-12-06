c278841d2c2e1dc6d290e58b5dba75eae9480dd1
nakaikento <okamoto2okamoto@gmail.com>
2019-12-06 23:01:43 +0900

    updated index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,23 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
+mongoose.set('useFindAndModify', false)
 const Note = require('../models/note')
 
 // middleware
 app.use(bodyParser.json())
 app.use(cors())
+app.use(express.static('build'))
+
+const requestLogger = ( request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:', request.path)
+  console.log('Body:', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
 app.use(express.static('build'))
 
 app.get('/', (req, res) => {
@@ -24,19 +36,42 @@ app.get('/api/notes', (req, res) => {
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id)
     .then(note => {
-      response.json(note.toJSON())
+      if (note) {
+        response.json(note.toJSON())
+      } else {
+      response.status(404).end()
+    }
+  })
+    .catch(error => next(error))
+})
+
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
     })
+    .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+const unknownEndpoint = ( request, response ) => {
+  response.status(404).send({ error: 'unknown endpoint'})
+}
 
-  response.status(204).end()
-})
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id'})
+  }
+  next(error)
+}
+
+app.use(errorHandler)
 
 const generateId = () => {
   const maxId = notes.length > 0
@@ -45,24 +80,19 @@ const generateId = () => {
   return maxId + 1
 }
 
-app.post('/api/notes', (request, response) => {
+app.put('/api/notes/:id', (request, response, next) => {
   const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing'
-    })
+  const note = {
+    content: body.content,
+    important: body.important,
   }
 
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-    data: new Date(),
-  })
-
-  note.save().then(savedNote => {
-    response.json(savedNote.toJSON())
-  })
+  Note.findByIdAndUpdate(request.params.id, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote.toJSON())
+    })
+    .catch( error => next(error))
 })
 
 const PORT = process.env.PORT || 3001
