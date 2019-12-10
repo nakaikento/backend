9dbba68a17ccfbd55e23f7f33596a834d07e00d3
nakaikento <okamoto2okamoto@gmail.com>
2019-12-10 10:16:54 +0900

    added User test

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -15,6 +15,36 @@ beforeEach(async () => {
   await Promise.all(promiseArray)
 })
 
+describe('when there is initially one user at db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+    const user = new User({ username: 'root', password: 'sekret'})
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const userAtStart = await helper.userInDb()
+
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen'
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const userAtEnd = await helper.usersInDb()
+    expect(usersAtEnd.length).toBe(usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    expect(usernames).toContain(newUser.username)
+  })
+})
+
 describe('when there is initially some notes saved', () => {
   test('notes are returned as json', async () => {
     await api
